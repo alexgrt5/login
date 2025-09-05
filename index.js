eb2b3c51811bec35147f9119b60a3630ad289ea3
alexgrt5 <alexgrt1703@gmail.com>
2025-09-05 17:49:20 -0600

    login complete

--- index.js
+++ index.js
@@ -15,28 +15,55 @@ app.use(session({
   saveUninitialized: true
 }));
 
-// BD Simulated
+// BD simulated
 const users = [
-    {
-        id: 1,
-        username: 'admin',
-        // password: '1234'
-        passwordHash: bcrypt.hashSync('1234', 10)
-    }
+  {
+    id: 1,
+    username: 'admin',
+    // Password: "1234"
+    passwordHash: bcrypt.hashSync('1234', 10)
+  }
 ];
 
 // Routes
 app.get('/', (req, res) => {
-    if (req.session.userId) {
-        res.send(`Hi, you are logged in as ${req.session.username}. <a href="/logout">Logout</a>`);
-    } else {
-        res.send (`
-            <h2>Login</h2>
-            <form method="POST" action="/login">
-                Username: <input name="username" /><br/>
-                Password: <input type="password" /><br/>
-                <button>Login</button>
-            </form>
-        `);
-    }
+  if (req.session.userId) {
+    res.send(`Hi, you are logged in as ${req.session.username}. <a href="/logout">Logout</a>`);
+  } else {
+    res.send(`
+      <h2>Login</h2>
+      <form method="POST" action="/login">
+        Username: <input name="username" /><br/>
+        Password: <input type="password" name="password" /><br/>
+        <button>Login</button>
+      </form>
+    `);
+  }
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(u => u.username === username);
+  if (!user) {
+    return res.send('User not found.');
+  }
+
+  const passwordMatch = bcrypt.compareSync(password, user.passwordHash);
+  if (!passwordMatch) {
+    return res.send('Invalid password..');
+  }
+
+  req.session.userId = user.id;
+  req.session.username = user.username;
+  res.redirect('/');
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
 });
