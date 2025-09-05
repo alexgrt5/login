00caa458990f07b940d0ffab5e7e1830eae6896b
alexgrt5 <alexgrt1703@gmail.com>
2025-09-05 17:35:15 -0600

    Routes

--- index.js
+++ index.js
@@ -24,3 +24,19 @@ const users = [
         passwordHash: bcrypt.hashSync('1234', 10)
     }
 ];
+
+// Routes
+app.get('/', (req, res) => {
+    if (req.session.userId) {
+        res.send(`Hi, you are logged in as ${req.session.username}. <a href="/logout">Logout</a>`);
+    } else {
+        res.send (`
+            <h2>Login</h2>
+            <form method="POST" action="/login">
+                Username: <input name="username" /><br/>
+                Password: <input type="password" /><br/>
+                <button>Login</button>
+            </form>
+        `);
+    }
+});
